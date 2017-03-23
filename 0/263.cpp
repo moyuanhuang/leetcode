f79188ff75a1af5aa1a32ce986455aa6901a26d0
moyuanhuang <moyuanhuang@gmail.com>
2017-03-22 23:21:04 -0700

    263.ugly number

---
+++ 0/263.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        while(num % 2 == 0)
+        	num /= 2;
+        while(num % 3 == 0)
+        	num	/= 3;
+        while(num % 5 == 0)
+        	num /= 5;
+        return num == 1;
+    }
+};
