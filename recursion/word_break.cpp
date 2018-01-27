0351dff2b22d9bcbb4f532eb096e32ad50fadf2a
moyuanhuang <moyuanhuang@gmail.com>
2018-01-26 16:41:15 -0800

    139-medium-Word Break

--- recursion/word_break.cpp
+++ recursion/word_break.cpp
@@ -1,11 +1,31 @@
+// leetcode 139. Work Break
+// use bfs is time consuming
+// use DP !!!
+
+// dp[i]: word(0, i - 1) can be broke
+// j : length of previous word
+// dp[i] = true if
+//   word(0, j - 1) can break and word(j, i) is in Dict for some j
+
+// i \in [0, s.size()]
+// j \in [0, i]
+
 class Solution {
 public:
-  vector<string> wordBreak(string s, vector<string>& wordDict) {
-    unordered_set<string> dict(wordDict.begin(), wordDict.end());
-    for(int i = 0; i < s.size(); ++i){
-      string prefix = s.substr(0, i + 1);
-      string subfix = s.substr(i + 1);
+    bool wordBreak(string s, vector<string>& wordDict) {
+      vector<bool> dp(s.size() + 1, false);
+      dp[0] = true;
+      unordered_set<string> uset(wordDict.begin(), wordDict.end());
 
+      for(int i = 0; i < s.size(); i++){
+          for(int j = i; j > 0; j--){
+              string word = s.substr(j, i - j + 1);
+              if(dp[j] && uset.count(word) > 0){
+                  dp[i + 1] = true;
+                  break;
+              }
+          }
+      }
+      return dp[s.size()];
     }
-  }
 };
