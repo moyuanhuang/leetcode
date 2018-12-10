2ba7a37dbd1578b40e074903187594a9c21efec8
moyuanhuang <moyuanhuang@gmail.com>
2018-12-09 19:04:10 -0800

    316-hard-Remove Duplicate Letters

--- strings/remove_duplicate_letters.py
+++ strings/remove_duplicate_letters.py
@@ -1,10 +1,6 @@
-# 316. Remove duplicate letters
+# 316. Remove Duplicate Letters
 # hard
-# 1. store the index of last occurrence of each letter, as "the last chance to keep this letter"
-# 2. divide the string by those indexes
-# 3. python char <-> int: ord() and chr()
 
-from heapq import *
 
 class Solution:
     def removeDuplicateLetters(self, s):
@@ -12,43 +8,44 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        last_occ = {}
-        res = ""
-        length = len(s)
-        
-        for i in range(length):
-            last_occ[s[i]] = i
-        heap = []
-        for k, v in last_occ.items():
-            heappush(heap, v)
+        lastOccOfChar = {}
+        for i, v in enumerate(s):
+            lastOccOfChar[v] = i
 
+        # splice the array
+        ends = []
+        for _, v in lastOccOfChar.items():
+            ends.append(v)
+        ends.sort()
+
+        result = ""
         start = 0
-        used = set()
-        while len(heap):
-            end = nsmallest(1, heap)[0]
+        usedChar = set()
+
+        while ends:
+            nextStart = None
+            end = ends[0]
             smallest = chr(ord('z') + 1)
+            smallestIndex = None
+
+            # find a smallest CHar in [start, end]
             for i in range(start, end + 1):
-                if s[i] in used:
+                ch = s[i]
+                if ch in usedChar:
                     continue
-                if s[i] < smallest:
-                    smallest = s[i]
-                    next_start = i + 1
+                if ch < smallest:
+                    smallest = ch
+                    smallestIndex = i
+                    nextStart = i + 1
 
-            res += smallest
-            self.findAndPop(heap, last_occ[smallest])
-            used.add(smallest)
-            start = next_start
+            # append the smallest char
+            result += smallest
+            usedChar.add(smallest)
 
-        return res
-    
-    # self implemented pq.remove(elem)
-    def findAndPop(self, heap, index):
-        temp = []
-        while len(heap):
-            elem = heappop(heap)
-            if elem == index:
-                break
-            temp.append(elem)
-        for elem in temp:
-            heappush(heap, elem)
+            # remove the lastOcc of the smallest element
+            lastOccIndex = lastOccOfChar[smallest]
+            ends.remove(lastOccIndex)
 
+            start = nextStart
+
+        return result
