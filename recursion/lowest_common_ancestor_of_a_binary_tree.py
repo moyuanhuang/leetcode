49df08d6591e02e17e158af81daf990575c4fe31
moyuanhuang <moyuanhuang@gmail.com>
2018-12-10 22:01:31 -0800

    236-medium-Lowest Common Ancestor of a Binary Tree

---
+++ recursion/lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,36 @@
+# 236. Lowest Common Ancestor of a Binary Tree
+# medium
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+
+        if root.val == p.val or root.val == q.val:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if not left and not right:
+            return None
+        elif left and right:
+            return root
+        else:
+            if left:
+                return left
+            else:
+                return right
